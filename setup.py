ba03a6ec41237b1ef8e909d3523a5aebe97a9135
Yifan Ai <ayfallen@hotmail.com>
2020-10-22 18:33:57 +1100

    Update version and include README file as long description

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (CWD / "README.md").read_text()
 
 setup(
     name='django-sales',
-    version='0.0.1',
+    version='0.1.0',
     description='Display different contact details based on query parameter and cookie',
     py_modules=['sales'],
     package_dir={'': 'src'},
@@ -16,7 +16,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    long_description='Display different contact details depending on query parameter',
+    long_description=README,
+    long_description_context_type='text/markdown',
     url='https://github.com/yifaneye/django-sales',
     author='Yifan Ai',
     author_email='yifanai@aol.com'
