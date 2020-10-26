a9d467d668a8dc3c79a3c3aba3a0bd25ee0cdd26
Yifan Ai <ayfallen@hotmail.com>
2020-10-27 02:07:23 +1100

    Change description content type, specify dependency, include packages, bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -7,17 +7,18 @@ README = (CWD / "README.rst").read_text()
 
 setup(
     name='django-sales',
-    version='0.1.0',
+    version='0.1.1',
     description='Display different contact details based on query parameter and cookie',
-    py_modules=['sales'],
-    package_dir={'': 'src'},
+    packages=["src/sales", "src/sales/middleware", "src/sales/templatetags"],
+    include_package_data=True,
+    install_requires=["Django"],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     long_description=README,
-    long_description_context_type='text/markdown',
+    long_description_content_type='text/x-rst',
     url='https://github.com/yifaneye/django-sales',
     author='Yifan Ai',
     author_email='yifanai@aol.com'
